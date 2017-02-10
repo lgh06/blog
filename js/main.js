b61e9fd120c4f8ec91c9d741bbca7aea8cc19098
lgh06 <hnnk@qq.com>
2017-02-10 10:40:57 +0800

    add disqus,add atom tern config file

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function($) {
+  //open image in new tab
   $(document).click(function(e){
     var $t = $(e.target),
         t = e.target;
@@ -9,4 +10,24 @@ jQuery(document).ready(function($) {
       a.click();
     }
   });
+
+  // add disqus
+  var $article = $('article');
+  $article.length &&
+  (function(){
+    $article.after('<div id="disqus_thread"></div>');
+    var disqus_config = function () {
+      this.page.url = window.location.href;
+      this.page.identifier = window.location.pathname;
+    };
+    window.disqus_config = disqus_config;
+    (function() {
+      var d = document, s = d.createElement('script');
+      s.src = '//hapleo.disqus.com/embed.js';
+      s.setAttribute('async','async');
+      s.setAttribute('data-timestamp', +new Date());
+      (d.head || d.body).appendChild(s);
+    })();
+  })();
+
 });
